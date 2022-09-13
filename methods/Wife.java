70dc8faac5f0b902ed711671860d8b2b2b732ebf
Sandeep Sonnad <sandeepssonnadxworks@gmail.com>
2022-09-13 10:51:42 +0530

    methods

---
+++ methods/Wife.java
@@ -0,0 +1,50 @@
+class Wife{
+	
+	static void cooking(){
+		System.out.println("Have a good cooking Skill");
+	}
+	static void bestFriend(){
+		System.out.println("Be like a Best Friend");
+	}
+	static void respect(){
+		System.out.println("Respect  Beliefs");
+	}
+	static void love(){
+		System.out.println("Show Love to Him");
+	}
+	static void support(){
+		System.out.println("Support Him");
+	}
+	static void faults(){
+		System.out.println("Accept Faults");
+	}
+	static void Romance(){
+		System.out.println("Don't Lose The Romance");
+	}
+	static void honesty(){
+		System.out.println("Be Honesty to Him");
+	}
+	static void silly(){
+		System.out.println("Don't be Silly");
+	}
+	static void believes(){
+		System.out.println("Believe him");
+	}
+	static void misunderstanding(){
+		System.out.println("Misunderstanding will Happens");
+	}
+	static void fight(){
+		System.out.println("Fight with him it also a Imp");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
