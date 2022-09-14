73f74e9437e41c48f9bccfdb875546985149a197
Sandeep Sonnad <sandeepssonnadxworks@gmail.com>
2022-09-14 21:38:47 +0530

    files

---
+++ MobileNumber.java
@@ -0,0 +1,56 @@
+class MobileNumber{
+	
+	static String getNameByMobile(long number)
+	{
+		
+		System.out.println("Entered Mobile Number");
+		if(number==9741426746L){
+			System.out.println("It is Sandeep Number");
+			return "sandeep";
+		}
+			
+			if(number==9741426747L){
+				System.out.println("It is Chetan Number");
+				return "yes";
+			}
+			if(number==9741426748L){
+				System.out.println("It is Sachin Number");
+				return "Sachya";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Manju Number");
+				return"manga";
+			}
+			if(number==9741426750L){
+				System.out.println("It is Manu Number");
+				return"manu";
+			}
+			if(number==9741426751L){
+				System.out.println("It is Krishna Number");
+				return"Krishna";
+			}
+			if(number==9741426752L){
+				System.out.println("It is Anusha Number");
+				return"Anu";
+			}
+			if(number==9741426753L){
+				System.out.println("It is Vanitha Number");
+				return"Vanitha";
+			}
+			if(number==9741426754L){
+				System.out.println("It is Madhu Number");
+				return"Madhu";
+			}
+			if(number==9741426755L){
+				System.out.println("It is Kushi Number");
+				return"Kushi";
+			}
+			if(number==9741426756L){
+				System.out.println("It is Basya Number");
+				return"Basavraj";
+			}
+				
+		 return null;	
+	}
+      	
+}
---
+++ MobileNumberRunner.java
@@ -0,0 +1,11 @@
+class MobileNumberRunner{
+	
+	public static void main(String[] no){
+		
+		String ref=MobileNumber.getNameByMobile(9741426751L);
+		System.out.println(ref);
+		
+	}
+	
+	
+}
